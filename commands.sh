b8595d442c2f6edddee5381a003272a05fe37c05
Diluk Angelo <dilukangelo@gmail.com>
2024-05-14 13:18:08 +0530

    removed logs as its 1gb plus

--- commands.sh
+++ commands.sh
@@ -3,13 +3,7 @@
 solana --version
 solana-keygen --version 
 
-pwd
-solana-keygen new --outfile /Users/dilukangelo/Documents/fiverr/solana/firstProject/Owner.json
-#pubkey: C9BzUWoWG7jsmTJX7vb3A6mUPYTvNB9vjZRZA2kU8yjU
-#nephew fire purse news divert slim cheap bundle nose level royal deliver
-solana-keygen new --outfile /Users/dilukangelo/Documents/fiverr/solana/firstProject/Creator.json
-#pubkey: 9XnkmS5dKdDkiAGfLnhUquc4VsmrwTsDKQaXJ9nRXJY7
-#team small unlock peace dad magic pencil animal mad tenant shrug day
+
 
 #set owner and devnet url https://api.devnet.solana.com
 solana config set --keypair ./Owner.json
@@ -51,9 +45,5 @@ sugar config update
 
 9Z1HDoAPw3mCAtjsEQ35Vzs4mJzZz9Enhb2zjRqYRKwd
 
-solana-keygen recover 'bus dwarf issue maze track phrase click plastic intact donate adjust about :?key=0/0' --outfile keypair.json
 
 
-
-
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyNDBkYTBjYS02OTA5LTQ2OWEtYWY4NS03NDE2YjVmOTc1OTYiLCJlbWFpbCI6ImRpbHVrYW5nZWxvQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIwZGM5NzczZmNiNGEwMTQxY2UyMiIsInNjb3BlZEtleVNlY3JldCI6ImM4MzdkMzMyMmQ1MjVkMDkwZDViYjU3MWNlYmM0YWU3MGExNDNkYTNmOWFmMjJkYjc2MjdhNGNlMGY0ZmQxZTQiLCJpYXQiOjE3MTQwMjkzODh9.HHmo7Wf3WsWzM6RMvH3uIdelaJvEsLDmcUT5yXs2PT0
